533999820e3d4efa275244b65037f055c73f7a08
hassaammgl <h455a4mmehtab@gmail.com>
2023-09-27 16:56:22 +0500

    adding to git

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import Header from './components/Header';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import "./style.css"
+import Home from './components/Home/Home';
 
 const App = () => {
   return (
     <Router>
       <Header />
       <Routes>
-        <Route exact path='/' element={<h1>Home Page</h1>} />
+        <Route exact path='/' element={<Home/>} />
       </Routes>
 
     </Router>
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,26 @@
+import { Center, Box,Grid,GridItem } from "@chakra-ui/react";
+
 
 const Home = () => {
   return (
-    <div>
-      
-    </div>
+    <Center display={"flex"}
+      alignItems={"center"}
+      flexDirection={"column"}
+      justifyContent={"center"}>
+
+      <Box
+        bgColor={"white"}
+        w={"70%"}
+        p={4}
+        border={"2px"}
+      >
+        <Grid templateColumns={'1fr 2fr'} templateRows={"600px 300px 300px"}>
+          <GridItem border={'1px'} width={'300px'}/>
+          <GridItem border={'1px'} width={'300px'}/>
+          <GridItem border={'1px'} width={'300px'}/>
+        </Grid>
+      </Box>
+    </Center>
   )
 }
 
